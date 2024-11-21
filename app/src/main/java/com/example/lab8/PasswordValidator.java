1a2a58640345b6ad2cc6e417663f711db585fbba
nstev009 <nstev009@uottawa.ca>
2024-11-21 15:20:04 -0500

    T

--- app/src/main/java/com/example/lab8/PasswordValidator.java
+++ app/src/main/java/com/example/lab8/PasswordValidator.java
@@ -7,13 +7,20 @@ public class PasswordValidator {
 
         boolean hasUpperCase = false;
         boolean hasLowerCase = false;
+        boolean hasDigit = false;
+        int counter = 0;
 
         for (char c : password.toCharArray()) {
             if (Character.isUpperCase(c)) hasUpperCase = true;
             if (Character.isLowerCase(c)) hasLowerCase = true;
+            if (Character.isDigit(c)) hasDigit = true;
+            counter++;
 
             // If both conditions are met, the password is valid
-            if (hasUpperCase && hasLowerCase) return true;
+
+        }
+        if (hasUpperCase && hasLowerCase && hasDigit && counter >= 10) {
+            return true;
         }
         return false;
     }
